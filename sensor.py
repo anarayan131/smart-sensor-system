0d2f8a2c63091272109b5fe82f13720108fab172
anarayan131 <67418730+anarayan131@users.noreply.github.com>
2025-06-10 10:33:00 +0200

    Update sensor.py
    Changed pressure unit from Bar to Pascal (Pa)

--- sensor.py
+++ sensor.py
@@ -5,5 +5,5 @@ while True:
     temp = round(random.uniform(20, 30), 2)
     pressure = round(random.uniform(1,5),3)
     print(f"Temperature: {temp} °C")
-    print(f"Pressure: {pressure} bar")
+    print(f"Pressure: {pressure} Pa")
     time.sleep(2)
