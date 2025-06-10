ce2caf1eb0c1b386539b93400f9611bf90337340
anarayan <anand.narayan@offis.de>
2025-06-10 10:26:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Hello, my first GIT tutorial. 
+For learning GIT, Docker, Kafka from chatgpt
---
+++ sensor.py
@@ -0,0 +1,9 @@
+import time
+import random
+
+while True:
+    temp = round(random.uniform(20, 30), 2)
+    pressure = round(random.uniform(1,5),3)
+    print(f"Temperature: {temp} °C")
+    print(f"Pressure: {pressure} bar")
+    time.sleep(2)
