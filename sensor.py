aa49aa3053868476262ef0001a4546a09054daf5
anarayan <anand.narayan@offis.de>
2025-06-10 16:07:49 +0200

    added non-buffering output

--- sensor.py
+++ sensor.py
@@ -4,6 +4,6 @@ import random
 while True:
     temp = round(random.uniform(20, 30), 2)
     pressure = round(random.uniform(1,5),3)
-    print(f"Temperature: {temp} °C")
-    print(f"Pressure: {pressure} Pa")
+    print(f"Temperature: {temp} °C", flush=True)
+    print(f"Pressure: {pressure} Pa", flush=True)
     time.sleep(2)
