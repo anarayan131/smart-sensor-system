1b85f6518866136dc6c8c49f0bb131972778e09b
anarayan <anand.narayan@offis.de>
2025-07-02 15:42:52 +0200

    all local to remote

--- sensor.py
+++ sensor.py
@@ -1,13 +1,22 @@
 import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
 import time
 import random
 
+# Publisher
+
+broker = "mosquitto"
+
+client = mqtt.Client()
+client.connect(broker, 1883, 60)
+
 while True:
     temp = round(random.uniform(20, 30), 2)
-    pressure = round(random.uniform(1,5),3)
+    pressure = round(random.uniform(1,5), 3)
 
-    publish.single("sensors/temp", f"{temp}", hostname="localhost")
+    client.publish("sensors/temp", f"{temp}")
     print(f"Sent T: {temp}", flush=True)
+
 
     # publish.single("sensors/pressure", f"{pressure}", hostname="localhost")
     # print(f"Sent P: {pressure}", flush=True)
--- subscriber.py
+++ subscriber.py
@@ -1,10 +1,15 @@
 import paho.mqtt.client as mqtt
 
+broker = "mosquitto"
+
+# Subscriber
+
+
 def on_message(client, userdata, msg):
-    print(f"Received: {msg.payload.decode()}")
+    print(f"Received message: {msg.payload.decode()}")
 
 client = mqtt.Client()
-client.connect("localhost")
+client.connect(broker, 1883, 60)
 client.subscribe("sensors/temp")
 
 client.on_message = on_message
