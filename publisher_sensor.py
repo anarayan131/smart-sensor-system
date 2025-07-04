28b878e3b5065762a4e4c7f83db66ff95e280652
anarayan <anand.narayan@offis.de>
2025-07-04 12:37:06 +0200

    working docker compose with MQTT and Mosquitto

--- publisher_sensor.py
+++ publisher_sensor.py
@@ -2,14 +2,31 @@ import paho.mqtt.publish as publish
 import paho.mqtt.client as mqtt
 import time
 import random
+import socket
 
 # Publisher
 
 broker = "mosquitto"
 
-client = mqtt.Client()
-client.connect(broker, 1883, 60)
+max_retries = 5
+wait_seconds = 2
 
+client = mqtt.Client()
+
+for attemp in range(max_retries):
+    try:
+        client.connect(broker, 1883, 60)
+        print("Connected to MQTT broker")
+        break #Successful connection
+    except (ConnectionRefusedError, socket.error):
+        print(f"Connection failed, retrying in {wait_seconds} seconds... ({attemp + 1}/{max_retries})")
+        time.sleep(wait_seconds)
+
+else:
+    print("Failed to connect to MQTT broker after multiple attempts.")
+    exit(1) 
+
+# Publish temperature and pressure data
 while True:
     temp = round(random.uniform(20, 30), 2)
     pressure = round(random.uniform(1,5), 3)
@@ -17,9 +34,7 @@ while True:
     client.publish("sensors/temp", f"{temp}")
     print(f"Sent T: {temp}", flush=True)
 
+    client.publish("sensors/pressure", f"{pressure}")
+    print(f"Sent P: {pressure}", flush=True)
 
-    # publish.single("sensors/pressure", f"{pressure}", hostname="localhost")
-    # print(f"Sent P: {pressure}", flush=True)
-
-    # print(f"Pressure: {pressure} Pa", flush=True)
     time.sleep(2)
--- subscriber.py
+++ subscriber.py
@@ -1,17 +1,35 @@
 import paho.mqtt.client as mqtt
+import time
+import socket
 
 broker = "mosquitto"
 
 # Subscriber
 
+max_retries = 5
+wait_seconds = 2
+
+client = mqtt.Client()
+
+for attemp in range(max_retries):
+    try:
+        client.connect(broker, 1883, 60)
+        print("Connected to MQTT broker")
+        break #Successful connection
+    except (ConnectionRefusedError, socket.error):
+        print(f"Connection failed, retrying in {wait_seconds} seconds... ({attemp + 1}/{max_retries})")
+        time.sleep(wait_seconds)
+
+else:
+    print("Failed to connect to MQTT broker after multiple attempts.")
+    exit(1) 
+
 
 def on_message(client, userdata, msg):
     print(f"Received message: {msg.payload.decode()}")
 
-client = mqtt.Client()
-client.connect(broker, 1883, 60)
-client.subscribe("sensors/temp")
 
+client.subscribe("sensors/temp")
 client.on_message = on_message
 
 # client.subscribe("sensors/pressure")
